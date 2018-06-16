31b0899acd253105834ff64dc24347eaac098efa
Andela-eugene <eugene.liyai@andela.com>
2018-06-17 00:50:46 +0300

    [Feat: #] pass position attribute

--- app/src/main/java/com/example/liyai/kotlin_intro/DataManager.kt
+++ app/src/main/java/com/example/liyai/kotlin_intro/DataManager.kt
@@ -24,6 +24,38 @@ object DataManager {
     }
 
     private fun initializeNotes() {
+        var noteInfo = NoteInfo(CourseInfo("android_intent", "Android Programing with Intent"),
+                "Android Intent",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
 
+        noteInfo = NoteInfo(CourseInfo("android_async", "Android Async Programing with Services"),
+                "Android Async",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
+
+        noteInfo = NoteInfo(CourseInfo("java_lang", "Java Fundamentals: The Java Language "),
+                "Java Fundamentals",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
+
+        noteInfo = NoteInfo(CourseInfo("java_core", "Java Fundamentals: The Core Platform "),
+                "Java Core",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
+        noteInfo = NoteInfo(CourseInfo("java_core", "Java Fundamentals: The Core Platform "),
+                "Core Deep dive",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
+
+        noteInfo = NoteInfo(CourseInfo("android_async", "Android Async Programing with Services"),
+                "Android Async 2",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
+
+        noteInfo = NoteInfo(CourseInfo("android_async", "Android Async Programing with Services"),
+                "Android Async 3",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit in neque eu facilisis")
+        notes.add(noteInfo)
     }
 }
